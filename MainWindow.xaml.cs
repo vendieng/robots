9e9e381d1982ff1821d45aee8e1b486891e49084
Vee <vendela.engblom@gmail.com>
2019-08-17 16:37:29 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Robotsssss
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Robot myRobot;
+
+        public MainWindow()
+        //skriv funktion för att skapa line and then call it
+        //for loop to create lines, use i for coordinates
+
+
+        {
+            InitializeComponent();
+            DrawLines();
+
+            myRobot = new Robot(0, 0, 8);
+            DrawRobot();
+
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+            if (e.Key == Key.Up)
+            {
+                myRobot.MoveUp();
+            }
+
+            else if (e.Key == Key.Down)
+                {
+                    myRobot.MoveDown();
+                }
+
+            else if (e.Key == Key.Right)
+            { 
+                myRobot.MoveRight();
+            }
+
+            else if (e.Key == Key.Left)
+            {
+                myRobot.MoveLeft();
+            }
+            //add else
+
+            DrawRobot();
+
+        }
+
+            private void DrawRobot()
+
+            {
+                
+                Canvas.SetLeft(robotimg, myRobot.X * 50);
+                Canvas.SetTop(robotimg, myRobot.Y * 50);
+
+            }
+
+
+
+
+        private void DrawLines()
+        //function to draw lines
+
+
+        {
+            for (int y = 0; y <= 400; y += 50)
+            {
+                Line line = new Line();
+                //creates new lines - line is a class, we've created a variable called line that has a new object of this class
+                line.X1 = 0;
+                line.Y1 = y;
+                line.X2 = 400;
+                line.Y2 = y;
+                line.Stroke = Brushes.Black;
+
+                //setting the starting point for the line
+
+                canvas.Children.Add(line);
+                //canvas is the name for the canvas, it has a child line, and then you add the line you made above
+            }
+
+            for (int x = 0; x <= 400; x += 50)
+            {
+                Line line = new Line();
+                //creates new lines - line is a class, we've created a variable called line that has a new object of this class
+                line.X1 = x;
+                line.Y1 = 0;
+                line.X2 = x;
+                line.Y2 = 400;
+                line.Stroke = Brushes.Black;
+
+                //setting the starting point for the line
+
+                canvas.Children.Add(line);
+                //canvas is the name for the canvas, it has a child line, and then you add the line you made above
+            }
+
+        }
+    }
+
+        
+    }
+
---
+++ Robot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robotsssss
+{
+    public class Robot
+    {
+        public int X { get; private set; } 
+        public int Y { get; private set; }
+        private int gridSize;
+
+        //private set so that the user can't set the position but it does set, get is called to find out what the position is
+
+        //creating attributes for the robot - here, position
+
+        public Robot(int x, int y, int gridSize)
+        {
+            this.X = x;
+            this.Y = y;
+            this.gridSize = gridSize;
+
+            //inside a class, this always refers to the attributes of the class
+        }
+
+        //create method to move robot in four directionsn
+
+        public void MoveUp()
+        {
+            if (Y >= 1)
+                //if y is more than or equal to 1 (because the grid is upside down) it moves up
+            {
+                Y--;
+            }
+        }
+
+        public void MoveDown()
+        {
+
+            if (Y < gridSize)
+            {
+                Y++;
+            }
+        }
+
+        public void MoveLeft()
+        {
+            if (X >= 1)
+            {
+                X--;
+            }
+        }
+
+        public void MoveRight()
+        {
+            if (X < gridSize)
+            {
+                X++;
+            }
+        }
+    }
+}
